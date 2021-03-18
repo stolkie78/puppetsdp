3ad2d46a60031a6f3e4bb91c3e48b31e21aba2da
Giedo Stolk <giedo@gsit.nl>
2021-03-18 22:52:18 +0100

    Disable anoying wizard

--- production/site-modules/jenkins/manifests/postconfig.pp
+++ production/site-modules/jenkins/manifests/postconfig.pp
@@ -7,12 +7,21 @@
 class jenkins::postconfig(
   Integer $port = '8080'
 ) {
-
+  # Configure Jenkins port
   file_line { "jenkins_port_${port}":
     path    => '/etc/sysconfig/jenkins',
     replace => true,
     line    => "JENKINS_PORT=\"${port}\"",
     match   => 'JENKINS_PORT=".*"',
+    notify  => Service['jenkins'],
+    require => Package['jenkins'],
+  }
+  # Disable annoying wizard
+  file_line { "jenkins_disable_wizard":
+    path    => '/etc/sysconfig/jenkins',
+    replace => true,
+    line    => 'JENKINS_JAVA_OPTIONS="-Djava.awt.headless=true -Djenkins.install.runSetupWizard=false"',
+    match   => 'JENKINS_JAVA_OPTIONS=".*"',
     notify  => Service['jenkins'],
     require => Package['jenkins'],
   }
